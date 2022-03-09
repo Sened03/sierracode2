042a7742f2ffecc354a721ba47c8e62491bb0ac9
bayti <bayti@baytis-MacBook-Pro.local>
2022-03-09 12:48:51 +0100

    eerste versie

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'donkersteBlauw': '#061E49',
+        'lichtBlauw':'#6EC1E4',
+      }
+    },
   },
   plugins: [],
 }
